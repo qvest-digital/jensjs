fa6dfe5c51c0c7a0a7125a97e52a0704e01b252f
mirabilos <t.glaser@tarent.de>
2023-01-06 16:30:30 +0100

    simple API server (beginnings)

--- produce/main.py
+++ produce/main.py
@@ -4,11 +4,44 @@ import os
 import sys
 
 import http.server
+import urllib.parse
 
+_JJS_path_registry = {}
 class JJSRequestHandler(http.server.SimpleHTTPRequestHandler):
     def send_response_only(self, code, message=None):
         super().send_response_only(code, message)
         self.send_header('Cache-Control', 'no-cache')
+
+    def do_GET(self):
+        u = urllib.parse.urlparse(self.path)
+        qs = urllib.parse.parse_qs(u.query, max_num_fields=20)
+        path = urllib.parse.unquote(u.path)
+        if path in _JJS_path_registry:
+            _JJS_path_registry[path](self, u, qs)
+        else:
+            super().do_GET()
+
+    def ez_rsp(self, body, code=200, ct=None):
+        if not isinstance(body, bytes):
+            body = bytes(body, "UTF-8")
+        self.send_response(code)
+        if ct is not None:
+            self.send_header("Content-Type", ct)
+        self.send_header("Content-Length", len(body))
+        self.end_headers()
+        self.wfile.write(body)
+
+def _Path(path, func):
+    _JJS_path_registry[path] = func
+    return func
+def Path(path):
+    return lambda func: _Path(path, func)
+
+@Path("/api/test")
+def API_Test(rh, u, qs):
+    print('u:', repr(u))
+    print('qs:', repr(qs))
+    rh.ez_rsp("Okäy!\n")
 
 # …
 
